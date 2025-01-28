42a7ea3442e6a047bc2cc4dff87eb3e83219fb66
Your Name <saghafy.80@gmail.com>
2025-01-28 21:37:02 +0330

    Add background image for home page

--- Front/src/pages/Home.jsx
+++ Front/src/pages/Home.jsx
@@ -1,5 +1,19 @@
+import { Fragment } from "react";
+import bg from "../assets/images/bg.jpg";
+
 function Home() {
-  return <h4>Home</h4>;
+  return (
+    <Fragment>
+      <img src={bg} alt="background" className="w-full h-full absolute object-cover" />
+      <div className="flex justify-center items-center min-h-[calc(100vh-40px)]">
+        <div className="py-2 px-3 rounded-2xl z-10">
+          <p className="font-semibold text-sm text-zinc-600">
+            Select Chat to Start Messaging
+          </p>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
 
 export default Home;

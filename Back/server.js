cd114d596fedbc8e2058444448f9a48af960df92
Your Name <saghafy.80@gmail.com>
2025-01-31 15:20:23 +0330

    Import app module to server.js file

--- Back/server.js
+++ Back/server.js
@@ -1,13 +1,11 @@
-const express = require("express");
 const http = require("http");
+const app = require("./app");
 const dotenv = require("dotenv");
 const socketIo = require("socket.io");
 
 dotenv.config({ path: "./config.env" });
 
-const { HOST, PORT } = process.env;
-
-const app = express();
+const { HOST, PORT = 3000 } = process.env;
 
 const server = http.createServer(app);
 

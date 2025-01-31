473d2fe6795701e9a0cd109b2c96428c9690563d
Your Name <saghafy.80@gmail.com>
2025-01-31 16:46:47 +0330

    Connect to local data base

--- Back/server.js
+++ Back/server.js
@@ -2,14 +2,19 @@ const http = require("http");
 const app = require("./app");
 const dotenv = require("dotenv");
 const socketIo = require("socket.io");
+const mongoose = require("mongoose");
 
 dotenv.config({ path: "./config.env" });
 
-const { HOST, PORT = 3000 } = process.env;
+const { HOST, PORT = 3000, DATABASE_CONNECTION } = process.env;
 
 const server = http.createServer(app);
 
 const io = socketIo(server);
+
+mongoose.connect(DATABASE_CONNECTION).then(() => {
+  console.log("Database connected successfully");
+});
 
 server.listen(PORT, HOST, () => {
   console.log(`Server is running on ${HOST} and port ${PORT}`);

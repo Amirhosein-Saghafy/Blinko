aa642f58172cc503e2bbe34b9df79c115ebad2f0
Your Name <saghafy.80@gmail.com>
2025-02-07 21:06:39 +0330

    Add route for signup operation

--- Back/routes/user.js
+++ Back/routes/user.js
@@ -4,5 +4,6 @@ const userControllers = require("../controllers/user");
 const router = express.Router();
 
 router.post("/login", userControllers.login);
+router.post("/signup", userControllers.signup);
 
 module.exports = router;

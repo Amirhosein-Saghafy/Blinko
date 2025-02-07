b4e8816d376ba4c9a51fcc9d3842e41359f54f42
Your Name <saghafy.80@gmail.com>
2025-02-07 18:50:55 +0330

    format line

--- Back/app.js
+++ Back/app.js
@@ -4,7 +4,7 @@ const user = require("./routes/user");
 
 const app = express();
 
-app.use(cors({ origin: "http://127.0.0.1:5173", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

244d1428f7ae55e566eca6a73d1148c002b11bc3
Your Name <saghafy.80@gmail.com>
2025-02-10 13:14:19 +0330

    Implement form submittion , using toaster package for showing the state of the login , checking the user state for navigating to home page , change the texts for login component

--- Front/src/pages/Login.jsx
+++ Front/src/pages/Login.jsx
@@ -1,8 +1,55 @@
+import { useActionState, useEffect } from "react";
 import login from "../assets/images/login.svg";
+import { LOGIN } from "../constant/urls";
+import toast, { Toaster } from "react-hot-toast";
+import store from "../store/store";
+import { loginUser } from "../store/userSlice";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
 
-const Login = ({ action }) => {
+const formActionHandler = async (previousState, formData) => {
+  toast.promise(
+    async () => {
+      const userName = formData.get("username");
+      const password = formData.get("password");
+      const requestBody = JSON.stringify({ userName, password });
+
+      const response = await fetch(LOGIN, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: requestBody,
+      });
+
+      const result = await response.json();
+
+      if (result.error) return Promise.reject(result.error);
+
+      store.dispatch(loginUser(result.data));
+      return result.message;
+    },
+    {
+      error: (err) => err,
+      loading: "Logging in...",
+      success: (msg) => msg,
+    }
+  );
+};
+
+const Login = () => {
+  const [state, submitAction] = useActionState(formActionHandler, null);
+  const userState = useSelector((state) => state.user);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (userState) navigate("/");
+  }, [userState, navigate]);
+
   return (
     <main className="bg-[#f8fafb]">
+      <Toaster />
       <div className="w-10/12 py-5 mx-auto">
         <div className="flex justify-center items-start h-[calc(100vh-40px)]">
           <div className="w-2/3 h-full">
@@ -15,17 +62,14 @@ const Login = ({ action }) => {
             </div>
           </div>
           <div className="w-1/3 mt-8">
-            <form action="" className="py-10 pl-5">
+            <form action={submitAction} className="py-10 pl-5">
               <div className="mb-3">
-                <h4 className="text-3xl">
-                  {action === "login" ? "Sign In" : "Sign Up"}
-                </h4>
+                <h4 className="text-3xl">Sign In</h4>
               </div>
               <div className="mb-5">
                 <p className="text-zinc-400">
-                  {action === "login"
-                    ? "Log in to access your chats and conversations anytime, seamlessly continuing where you left off!"
-                    : "Sign up to create an account and securely access your chats and conversations anytime!"}
+                  Log in to access your chats and conversations anytime,
+                  seamlessly continuing where you left off!
                 </p>
               </div>
               <div className="mb-5">
@@ -42,39 +86,37 @@ const Login = ({ action }) => {
                   placeholder="password"
                 />
               </div>
-              {action === "login" && (
-                <>
-                  <div className="mb-3 flex justify-between items-center">
-                    <div className="flex items-center">
-                      <input
-                        type="checkbox"
-                        name="remember"
-                        id="remember"
-                        className=""
-                      />
-                      <label
-                        htmlFor="remember"
-                        className="text-zinc-500 text-sm ml-2"
-                      >
-                        Remember me
-                      </label>
-                    </div>
-                    <div>
-                      <a
-                        href="#"
-                        className="text-xs text-zinc-500 decoration-solid decoration-1 underline underline-offset-2"
-                      >
-                        Forgot Password
-                      </a>
-                    </div>
-                  </div>
-                </>
-              )}
+
+              <div className="mb-3 flex justify-between items-center">
+                <div className="flex items-center">
+                  <input
+                    type="checkbox"
+                    name="remember"
+                    id="remember"
+                    className=""
+                  />
+                  <label
+                    htmlFor="remember"
+                    className="text-zinc-500 text-sm ml-2"
+                  >
+                    Remember me
+                  </label>
+                </div>
+                <div>
+                  <a
+                    href="#"
+                    className="text-xs text-zinc-500 decoration-solid decoration-1 underline underline-offset-2"
+                  >
+                    Forgot Password
+                  </a>
+                </div>
+              </div>
+
               <button
                 type="submit"
                 className="my-8 w-full py-4 bg-[#6c63ff] hover:bg-[#635cec] text-white outline-none rounded-md"
               >
-                {action === "login" ? "Log In" : "Sign Up"}
+                Log In
               </button>
             </form>
           </div>

35407c0ef6971ecf2f62ca94686b99e08dc25a20
Your Name <saghafy.80@gmail.com>
2025-02-10 13:12:26 +0330

    Separate the signup from login component page , implement the form submittion , use user state for navigating to home page

---
+++ Front/src/pages/Signup.jsx
@@ -0,0 +1,103 @@
+import { useActionState, useEffect } from "react";
+import login from "../assets/images/login.svg";
+import { SIGNUP } from "../constant/urls";
+import toast, { Toaster } from "react-hot-toast";
+import store from "../store/store";
+import { loginUser } from "../store/userSlice";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
+
+const formActionHandler = async (previousState, formData) => {
+  toast.promise(
+    async () => {
+      const userName = formData.get("username");
+      const password = formData.get("password");
+      const requestBody = JSON.stringify({ userName, password });
+
+      const response = await fetch(SIGNUP, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: requestBody,
+      });
+
+      const result = await response.json();
+
+      if (result.error) return Promise.reject(result.error);
+
+      store.dispatch(loginUser(result.data));
+      return result.message;
+    },
+    {
+      error: (err) => err,
+      loading: "Creating profile...",
+      success: (msg) => msg,
+    }
+  );
+};
+
+const Signup = () => {
+  const [state, submitAction] = useActionState(formActionHandler, null);
+  const userState = useSelector((state) => state.user);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (userState) navigate("/");
+  }, [userState, navigate]);
+
+  return (
+    <main className="bg-[#f8fafb]">
+      <Toaster />
+      <div className="w-10/12 py-5 mx-auto">
+        <div className="flex justify-center items-start h-[calc(100vh-40px)]">
+          <div className="w-2/3 h-full">
+            <div className="w-full h-full py-10 pr-32">
+              <img
+                src={login}
+                alt="login-illustration"
+                className="w-full h-full object-cover"
+              />
+            </div>
+          </div>
+          <div className="w-1/3 mt-8">
+            <form action={submitAction} className="py-10 pl-5">
+              <div className="mb-3">
+                <h4 className="text-3xl">Sign Up</h4>
+              </div>
+              <div className="mb-5">
+                <p className="text-zinc-400">
+                  Sign up to create an account and securely access your chats
+                  and conversations anytime!
+                </p>
+              </div>
+              <div className="mb-5">
+                <input
+                  type="text"
+                  name="username"
+                  className="py-5 px-4 bg-gray-100 rounded-t-lg w-full outline-none text-sm placeholder:text-zinc-400 text-zinc-400 border-b border-b-gray-200"
+                  placeholder="Username"
+                />
+                <input
+                  type="password"
+                  name="password"
+                  className="py-5 px-4 bg-gray-100 rounded-b-xl w-full outline-none text-sm placeholder:text-zinc-400 text-zinc-400"
+                  placeholder="password"
+                />
+              </div>
+              <button
+                type="submit"
+                className="my-8 w-full py-4 bg-[#6c63ff] hover:bg-[#635cec] text-white outline-none rounded-md"
+              >
+                Sign Up
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Signup;

c2613de3c1641ca481a4ba368bf3e3286daadcf4
Your Name <saghafy.80@gmail.com>
2025-02-11 23:55:57 +0330

    ''

--- Front/src/ui/Loader.jsx
+++ Front/src/ui/Loader.jsx
@@ -1,8 +1,8 @@
 function Loader() {
   return (
     <>
-      <div className="fixed top-0 left-0 w-screen h-dvh z-[1000] bg-gray-200 opacity-70"></div>
-      <div className="absolute left-[calc(50%-28px)] top-[calc(50%-28px)] w-14 h-14 border-4 border-green-400 border-b-transparent rounded-full animate-spin z-[1001]"></div>
+      <div className="fixed top-0 left-0 w-screen h-dvh z-[1000] bg-slate-900 opacity-70"></div>
+      <div className="absolute left-[calc(50%-28px)] top-[calc(50%-28px)] w-11 h-11 border-4 border-slate-400 border-b-transparent rounded-full animate-spin z-[1001]"></div>
     </>
   );
 }

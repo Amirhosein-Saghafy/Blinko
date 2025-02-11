49b16da18e814a2c0c97af3d40c8ee17ab48f17f
Your Name <saghafy.80@gmail.com>
2025-02-11 23:45:31 +0330

    Reformat the img tag

--- Front/src/pages/Home.jsx
+++ Front/src/pages/Home.jsx
@@ -4,7 +4,11 @@ import bg from "../assets/images/bg.jpg";
 function Home() {
   return (
     <Fragment>
-      <img src={bg} alt="background" className="w-full h-full absolute object-cover" />
+      <img
+        src={bg}
+        alt="background"
+        className="w-full h-full absolute object-cover"
+      />
       <div className="flex justify-center items-center min-h-[calc(100vh-40px)]">
         <div className="py-2 px-3 rounded-2xl z-10">
           <p className="font-semibold text-sm text-zinc-600">

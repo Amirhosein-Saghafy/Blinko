e7f83d9e4131d6d359f944bea4dc47dbe5947946
Your Name <saghafy.80@gmail.com>
2025-02-11 23:56:53 +0330

    Add new routes to url file

--- Front/src/constant/urls.js
+++ Front/src/constant/urls.js
@@ -1,5 +1,7 @@
 const BASE_URL = "http://127.0.0.1:8000";
 const LOGIN = BASE_URL + "/user/login";
 const SIGNUP = BASE_URL + "/user/signup";
+const GET_ALL_USERS = BASE_URL + "/user/all";
+const GET_ALL_CHATS = BASE_URL + "/chat/all";
 
-export { LOGIN, SIGNUP };
+export { LOGIN, SIGNUP, GET_ALL_CHATS, GET_ALL_USERS };

17f8bd2b6f16288c66fe4c61b64269908dc67259
Your Name <saghafy.80@gmail.com>
2025-03-14 16:53:09 +0330

    Implement get all chats for a user and apply authentication middleware to it

--- Back/controllers/chat.js
+++ Back/controllers/chat.js
@@ -1,3 +1,24 @@
-exports.getAll = async (req, res, next) => {
-  res.sendStatus(200);
+const chatModel = require("../models/chat");
+const userModel = require("../models/user");
+
+exports.getAll = async (req, res) => {
+  const user = await userModel.findOne({
+    userName: req.userName,
+    password: req.password,
+  });
+  if (!user) {
+    return res.status(404).json({ error: "User not found" });
+  }
+
+  try {
+    const chats = await chatModel.find({
+      $or: [{ senderId: user._id }, { receiverId: user._id }],
+    });
+
+    res.status(200).json({
+      data: chats,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
--- Back/routes/chat.js
+++ Back/routes/chat.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const chatController = require("../controllers/chat");
+const authController = require("../controllers/auth");
 
 const router = express.Router();
+
+router.use(authController.authentication);
 
 router.get("/all", chatController.getAll);
 

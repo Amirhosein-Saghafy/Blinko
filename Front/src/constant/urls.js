8a3af2e851b72233aca7700fff338ece9e7386e5
Your Name <saghafy.80@gmail.com>
2025-03-14 23:05:13 +0330

    Add send message url to urls file

--- Front/src/constant/urls.js
+++ Front/src/constant/urls.js
@@ -4,5 +4,6 @@ const SIGNUP = BASE_URL + "/user/signup";
 const PROFILE = BASE_URL + "/user/profile";
 const GET_ALL_USERS = BASE_URL + "/user/all";
 const GET_ALL_CHATS = BASE_URL + "/chat/all";
+const SEND_MESSAGE = BASE_URL + '/chat/send';
 
-export { LOGIN, SIGNUP, GET_ALL_CHATS, GET_ALL_USERS, PROFILE };
+export { LOGIN, SIGNUP, GET_ALL_CHATS, GET_ALL_USERS, PROFILE, SEND_MESSAGE };

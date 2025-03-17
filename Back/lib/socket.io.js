3844ac3bb87b922237918a961ac69e87eca882a8
Your Name <saghafy.80@gmail.com>
2025-03-17 14:03:25 +0330

    create a list for storing connected users and checking for duplication, emit an event when some new guy connects to server

--- Back/lib/socket.io.js
+++ Back/lib/socket.io.js
@@ -9,9 +9,22 @@ const io = new Server(server, {
   connectionStateRecovery: {},
 });
 
+const users = {};
+
 io.on("connection", (socket) => {
+  const isConnected = Object.keys(users).some((userId) => {
+    return userId === socket.handshake.auth.userId;
+  });
+
+  if (isConnected) return;
+
+  users[socket.handshake.auth.userId] = socket.id;
+
+  io.emit("updateOnlineUsers", users);
+
   console.log(`A user connected : ${socket.id}`);
   socket.on("disconnect", () => {
+    delete users[socket.handshake.auth.userId];
     console.log(`User disconnected : ${socket.id}`);
   });
 });

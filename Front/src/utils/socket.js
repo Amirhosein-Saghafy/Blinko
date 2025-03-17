4183a5ef2ea58f360f4238a3fb86860322ff2b51
Your Name <saghafy.80@gmail.com>
2025-03-17 14:37:09 +0330

    Dispatch new onlineUsersList

--- Front/src/utils/socket.js
+++ Front/src/utils/socket.js
@@ -1,9 +1,13 @@
 import { io } from "socket.io-client";
+import store from "../store/store";
+import { updateOnlineUsersList } from "../store/chatSlice";
 
 function connect(userId) {
   const socket = io("http://127.0.0.1:8000", { auth: { userId } });
 
   socket.on("updateOnlineUsers", (users) => {
+    store.dispatch(updateOnlineUsersList(users));
+
     console.log(users);
   });
 }

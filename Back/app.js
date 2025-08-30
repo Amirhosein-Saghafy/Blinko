7e01e87f7b47ceb35f32ddc2ec5af5a2f096d117
Your Name <saghafy.80@gmail.com>
2025-08-31 00:00:06 +0330

    Serve built files in root path and redirect all invalid requests to root path so the built index file would be serve

--- Back/app.js
+++ Back/app.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.static("./uploads"));
+app.use(express.static("./build"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,5 +16,8 @@ app.use(cookieParser());
 
 app.use("/user", user);
 app.use("/chat", chat);
+app.all("*", (req, res) => {
+  res.redirect("/");
+});
 
 module.exports = app;

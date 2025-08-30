685850a386d79e5e1de8575c60ab15c64fb26317
Your Name <saghafy.80@gmail.com>
2025-08-31 00:10:32 +0330

    Add correct config socket for production

--- Front/src/utils/socket.js
+++ Front/src/utils/socket.js
@@ -6,6 +6,7 @@ let socket = null;
 
 function connect(userId) {
   socket = io("http://127.0.0.1:8000", { auth: { userId } });
+  // socket = io("", { auth: { userId } });
 
   socket.on("updateOnlineUsers", (users) => {
     store.dispatch(updateOnlineUsersList(users));

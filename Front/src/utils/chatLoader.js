b1774c0859eb01a52bcb4e1fdca548ea8246e4f0
Your Name <saghafy.80@gmail.com>
2025-09-01 14:20:47 +0330

    Move git repo to build folder

--- Front/src/utils/chatLoader.js
+++
@@ -1,9 +0,0 @@
-import { GET_ALL_CHATS } from "../constant/urls";
-
-function loader({ params }) {
-  return fetch(GET_ALL_CHATS + "/" + params.id, {
-    credentials: "include",
-  }).then((response) => response.json());
-}
-
-export default loader;
